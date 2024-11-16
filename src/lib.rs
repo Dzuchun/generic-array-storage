e632e7de208ad1e9f28ef008de284cd33a9baf51
Dzuchun <Infernodambik@gmail.com>
2024-11-16 13:04:54 +0200

    Useful type aliases

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,6 @@
 #![doc = include_str!("../README.md")]
 #![no_std] // <-- yeah, in case you wondered - there you are, feel free to use it
+
+type TNum<const N: usize> = typenum::Const<N>;
+type AlgNum<const N: usize> = nalgebra::Const<N>;
+type ArrLen<const N: usize> = <TNum<N> as generic_array::IntoArrayLength>::ArrayLength;
