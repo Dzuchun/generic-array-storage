06988fb3c302e800a46f69febcffd36d2ad7d1c6
Dzuchun <Infernodambik@gmail.com>
2024-11-16 13:06:09 +0200

    `GenericArrayStorage` definition and `Copy` impl

--- src/lib.rs
+++ src/lib.rs
@@ -4,3 +4,21 @@
 type TNum<const N: usize> = typenum::Const<N>;
 type AlgNum<const N: usize> = nalgebra::Const<N>;
 type ArrLen<const N: usize> = <TNum<N> as generic_array::IntoArrayLength>::ArrayLength;
+
+use generic_array::{ArrayLength, GenericArray};
+/// A stack-allocated storage, of [`typenum`]-backed row-major two dimensional array
+#[derive(Debug, Clone)]
+pub struct GenericArrayStorage<T, R, C>(GenericArray<GenericArray<T, R>, C>)
+where
+    R: ArrayLength,
+    C: ArrayLength;
+
+impl<T, R, C> Copy for GenericArrayStorage<T, R, C>
+where
+    T: Copy,
+    R: ArrayLength,
+    R::ArrayType<T>: Copy,
+    C: ArrayLength,
+    C::ArrayType<GenericArray<T, R>>: Copy,
+{
+}
